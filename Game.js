f1a8b4be0ea7ec93b6744e07dea868ae2342438a
root <root@DESKTOP-6B3FQL8.localdomain>
2022-06-09 12:51:10 -0500

    added stlye

--- Game.js
+++ Game.js
@@ -66,7 +66,7 @@ if(questionTwo==anw2){
                     if (questionSix == 4) {
                         //Will add 1 to current score
                         score = score + 1
-                        alert("You got it right! Your score is:" + score);
+                        alert("You got it right! Your score is: " + score);
                         break;
                     } else if (questionSix > 4) {
                       alert("Your guess was " + question + ". That's too high. Try Again!")
@@ -75,6 +75,7 @@ if(questionTwo==anw2){
                    }
                  }
                    for(let i = 0; i<6; i++){
+                       //all the right choices
                     let num = /['1', '3', '5', '7' , '9']/;
                     let question = prompt('What is one odd number from these choices : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10');
                     if(question.match(num)){
@@ -88,110 +89,9 @@ if(questionTwo==anw2){
 }
 game() 
 
-// let point = "0"
-// function game2(){
-//     for(let i = 0;i<4; i++){
-//         let question = prompt('Guess a number between 1 and 10')
-//         if(question === 4){
-//             alert("Correct!! You have" + points);
-//             break;
-//         }else if(question > 4);{
-//         alert("Hmm your to high")
-//         }else if(question < 4);{
-//             alert('You are to low')
-//         }
-//         }
-//     }
-//         game2()   
-
-// let point = "0"
-// function game2() {
-//     // let score = 0
-//     for(let i = 0;i<4; i++){
-//     let question = prompt('Guess a number between 1 and 10')
-//     if (question == 4) {
-//         score = score + 1
-//         alert("You got it right! Your score is:" + score);
-//         break;
-//     } else if (question > 4) {
-//       alert("Your guess was " + question + ". That's too high. Try Again!")
-//     } else if (question < 4) {
-//      alert("Your guess was " + question + ". That's too low. Try Again!")
-//    }
-//  }
-// }
-// game2()
-
-// function game2 (){
-//     for(let m = 0;m<6; m++){
-//         let question = prompt('What is one odd number for 1 to 10')
-//         let awn7 = ['1' , '3', '5', '7', '9'];
-//     }if(awn7 === question){
-//         alert('Correct!!');
-//         }
-//         else{
-//           alert("Wrong");
-//         }
-//     }
-//     game2()
 
 
-// function game3(){
-//     for (let i = 0; i<6; i++);
-// let Answers = ["1,3,5,7,9"];
-// let question = prompt('What is one odd number for 1 to 10');
-//  {
-//   if (question === Answers)
-// {alert("Correct!!")}
-// else {alert("Wrong")
-//   // unacceptable answer code
-// }
-// }
-// }
-// game3()
 
 
-// function game(){
-//     for(let i = 0; i<6; i++){
-//  let question = prompt('animals');
-//  let anw7 = ("dog");
-//  if(question == anw7){
-//      alert("correct")
-//      break;
-//  }else{
-//      alert('wrong')
-//  }
-// }
-// }
-// game()
 
-
-// function (){
-//     let num = ['1', '3', '5'];
-//     let question = prompt('1 to 10');
-//     // for(let m = 0; m<6; m++)
-//     for(let i = 0; i<num.length; i++){
-//         if(question === num[i])
-//         result = true;
-//     }
-//     if(result){
-//         alert("right");
-//     }else{
-//         alert("no")
-// }
-// }
-
-// function game2(){
-//     for(let i = 0; i<6; i++){
-// let num = /['1', '3', '5', '7' , '9']/;
-// let question = prompt('Guess a odd number between 1 and 10');
-// if(question.match(num)){
-//     score = score + 1
-//     alert('yup')
-//     break;
-// }else{
-//     alert('nope')
-// }
-//  }
-// }
-// game2()
+//qi[0]=="y" || q1[0]=="Y"
